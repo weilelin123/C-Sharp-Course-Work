9cc79265de9f7e4877cd44e7e5b52b81a6dd1ff8
Lok <42785367+weilelin123@users.noreply.github.com>
2023-03-20 21:19:20 -0700

    Upload 'Indexers' folder

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,25 @@
+﻿namespace MyApp
+{
+    internal partial class Program
+    {
+        public class HttpCookie
+        {
+            private readonly Dictionary<string, string> _dictionary;
+
+            public HttpCookie()
+            {
+                _dictionary = new Dictionary<string, string>();
+            }
+
+            public string this[string key]
+            {
+                get { return _dictionary[key]; }
+                set
+                {
+                    _dictionary[key] = value;
+                }
+
+            }
+        }
+    }
+ }
---
+++ Indexers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "test";
+            Console.WriteLine(cookie["name"]);
+
+
+            Console.ReadKey();
+        }
+    }
+}
