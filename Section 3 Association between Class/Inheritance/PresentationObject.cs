0b2d234766646111faa5f9bc33755378180ff8f8
Lok <linweile@hawaii.edu>
2023-03-21 11:38:47 -0700

    Add 'Inheritance' Folder
    Explain what is Inheritance

---
+++ Section 3 Association between Class/Inheritance/PresentationObject.cs
@@ -0,0 +1,21 @@
+﻿namespace MyApp
+{
+    internal partial class Program
+    {
+        public class PresentationObject
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public void Copy()
+            {
+                Console.WriteLine("Object copied");
+            }
+
+            public void Duplicate()
+            {
+                Console.WriteLine("Object duplicate");
+            }
+        }
+    }
+}
---
+++ Section 3 Association between Class/Inheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+
+namespace MyApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+
+            Console.ReadKey();
+        }
+    }
+}
