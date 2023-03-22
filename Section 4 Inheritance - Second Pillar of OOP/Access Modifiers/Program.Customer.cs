332d421a30044436d86fb14e06c85a3686d06bf2
Lok <linweile@hawaii.edu>
2023-03-22 16:29:24 -0700

    Create a new section

---
+++ Section 4 Inheritance - Second Pillar of OOP/Access Modifiers/Program.Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace MyApp
+{
+    internal partial class Program
+    {
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public void Promote()
+            {
+                var rating = CalculateRating(excludeOrders: true);
+                if (rating == 0)
+                    Console.WriteLine("Promoted to Level 1");
+                else
+                    Console.WriteLine("Promoted to Level 2");
+            }
+
+            public int CalculateRating(bool excludeOrders)
+            {
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ Section 4 Inheritance - Second Pillar of OOP/Access Modifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+
+namespace MyApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            String[,] parkingLot = { { "Mustang", "F-150", "Explorer" },
+                                     { "Corvette", "Camaro", "Silverado" },
+                                     { "Corolla", "Camry", "Rav4" }
+                                   };
+
+            for(int i = 0; i < parkingLot.GetLength(0); i ++)
+            {
+                for (int j = 0; j < parkingLot.GetLength(1); j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+           }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
