4f2ec965fc014f84075f695c74ff7bffdf372856
Lok <linweile@hawaii.edu>
2023-03-29 16:54:54 -0700

    Upload boxing/unboxing

---
+++ Section 4 Inheritance - Second Pillar of OOP/Boxing:Unboxing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Formats.Tar;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+
+namespace MyApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Text();
+            Text text = (Text)shape;
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
